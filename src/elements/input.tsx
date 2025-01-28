17bfcc97cac5e638fe37d92f2aa2c425ec52eb0d
ilena2205 <160659498+ilena2205@users.noreply.github.com>
2025-01-28 13:28:01 +0300

    Delete src/elements/input.tsx

--- src/elements/input.tsx
+++
@@ -1,9 +0,0 @@
-import { FileInput } from '@/components/ui/input';
-
-export function FileInputDemo() {
-	return (
-		<div className="grid w-full max-w-sm items-center gap-1.5">
-			<FileInput id="picture" type="file" />
-		</div>
-	);
-}
