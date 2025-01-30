be6ce8dd51a421529fdaeecfc6c9145d8f8599c4
Anastasia Ivankina <anst.iv@yandex.ru>
2025-01-30 20:30:03 +0300

    feat(tailwind.config.js / index.scss): удалить лишние переменные

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,8 +29,6 @@ export default {
 					transparent48: 'hsl(from var(--primary-main) h s l / 48%)',
 					gradientMain: 'linear-gradient(135deg, #1563FF 0%, #0D44B4 100%)',
 					gradientBold: 'linear-gradient(135deg, #024BDD 0%, #053CAA 100%)',
-					DEFAULT: 'hsl(var(--primary))',
-					foreground: 'hsl(var(--primary-foreground))',
 				},
 				secondary: {
 					lighter: '#EFD6FF',
@@ -47,8 +45,6 @@ export default {
 					transparent48: 'hsl(from var(--secondary-main) h s l / 48%)',
 					gradientMain: 'linear-gradient(135deg, #C684FF 0%, #8E33FF 100%)',
 					gradientBold: 'linear-gradient(135deg, #8E33FF 0%, #5119B7 100%)',
-					DEFAULT: 'hsl(var(--secondary))',
-					foreground: 'hsl(var(--secondary-foreground))',
 				},
 				info: {
 					lighter: '#CAFDF5',
@@ -146,38 +142,6 @@ export default {
 					whiteTransparent24: 'hsl(from var(--common-white) h s l / 24%)',
 					whiteTransparent32: 'hsl(from var(--common-white) h s l / 32%)',
 					whiteTransparent48: 'hsl(from var(--common-white) h s l / 48%)',
-				},
-				background: 'hsl(var(--background))',
-				foreground: 'hsl(var(--foreground))',
-				card: {
-					DEFAULT: 'hsl(var(--card))',
-					foreground: 'hsl(var(--card-foreground))',
-				},
-				popover: {
-					DEFAULT: 'hsl(var(--popover))',
-					foreground: 'hsl(var(--popover-foreground))',
-				},
-				muted: {
-					DEFAULT: 'hsl(var(--muted))',
-					foreground: 'hsl(var(--muted-foreground))',
-				},
-				accent: {
-					DEFAULT: 'hsl(var(--accent))',
-					foreground: 'hsl(var(--accent-foreground))',
-				},
-				destructive: {
-					DEFAULT: 'hsl(var(--destructive))',
-					foreground: 'hsl(var(--destructive-foreground))',
-				},
-				border: 'hsl(var(--border))',
-				input: 'hsl(var(--input))',
-				ring: 'hsl(var(--ring))',
-				chart: {
-					1: 'hsl(var(--chart-1))',
-					2: 'hsl(var(--chart-2))',
-					3: 'hsl(var(--chart-3))',
-					4: 'hsl(var(--chart-4))',
-					5: 'hsl(var(--chart-5))',
 				},
 			},
 			fontFamily: {
