f2fe609e119fc0d0a6969f69fc8b1562bc73f39f
Talissa <liayai@ya.ru>
2025-02-01 00:35:56 +0300

    fix: hover behavior

--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/lib/utils';
 
 const switchVariants = cva(
-	'peer inline-flex shrink-0 cursor-pointer items-center rounded-full transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-1 focus-visible:ring-offset-background data-[state=unchecked]:bg-grey-transparent48 disabled:cursor-not-allowed  disabled:opacity-[0.48]',
+	'group inline-flex shrink-0 cursor-pointer items-center rounded-full transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-1 focus-visible:ring-offset-background data-[state=unchecked]:bg-grey-transparent48  disabled:opacity-[0.48] disabled:pointer-events-none',
 	{
 		variants: {
 			variant: {
@@ -51,17 +51,16 @@ const Switch = React.forwardRef<
 	>
 		<SwitchPrimitives.Thumb
 			className={cn(
-				'pointer-events-none block ring-0 rounded-full bg-white transition-transform data-[state=unchecked]:translate-x-[3px] data-[state=unchecked]:ring-grey-500/[.08]',
+				'pointer-events-none block ring-0 rounded-full bg-white transition-transform data-[state=unchecked]:translate-x-[3px] group-hover:ring-grey-800/[.08] group-hover:ring-grey-800/[.08]',
 				size === 'small'
-					? 'w-[10px] h-[10px] data-[state=checked]:translate-x-[12px] hover:ring-[6px]'
-					: 'w-[14px] h-[14px] data-[state=checked]:translate-x-[16px] hover:ring-[12px]',
-				variant === 'default' ? 'data-[state=checked]:ring-grey-800/[.08]': '',
-				variant === 'primary' ? 'data-[state=checked]:ring-primary-main/[.08]': '',
-				variant === 'secondary' ? 'data-[state=checked]:ring-secondary-main/[.08]': '',
-				variant === 'info' ? 'data-[state=checked]:ring-info-main/[.08]': '',
-				variant === 'success' ? 'data-[state=checked]:ring-success-main/[.08]': '',
-				variant === 'warning' ? 'data-[state=checked]:ring-warning-main/[.08]': '',
-				variant === 'error' ? 'data-[state=checked]:ring-error-main/[.08]': '',
+					? 'w-[10px] h-[10px] data-[state=checked]:translate-x-[12px] group-hover:ring-[8px]'
+					: 'w-[14px] h-[14px] data-[state=checked]:translate-x-[16px] group-hover:ring-[12px]',
+				variant === 'primary' ? 'data-[state=checked]:group-hover:ring-primary-main/[.08]': '',
+				variant === 'secondary' ? '	data-[state=checked]:group-hover:ring-secondary-main/[.08]': '',
+				variant === 'info' ? 'data-[state=checked]:group-hover:ring-info-main/[.08]': '',
+				variant === 'success' ? 'data-[state=checked]:group-hover:ring-success-main/[.08]': '',
+				variant === 'warning' ? 'data-[state=checked]:group-hover:ring-warning-main/[.08]': '',
+				variant === 'error' ? 'data-[state=checked]:group-hover:ring-error-main/[.08]': '',
 			)}
 		/>
 	</SwitchPrimitives.Root>
