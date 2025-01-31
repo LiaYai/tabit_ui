7d39410f5bfcef3aebee85d81563caf67a316a8b
Ulyana Kulbida <unikub@yandex.ru>
2025-01-31 20:23:23 +0600

    feat(index): reindexing icon imports [#44267263]

---
+++ icons/index.ts
@@ -0,0 +1,7 @@
+import { ReactComponent as DotIcon } from '@icons/DotIcon.svg';
+import { ReactComponent as ReplaceIcon } from './ReplaceIcon.svg';
+import { ReactComponent as StarIcon } from './StarIcon.svg';
+import { ReactComponent as TrendingUpIcon } from './TrendingUpIcon.svg';
+import { ReactComponent as WindowsIcon } from './WindowsIcon.svg';
+
+export { DotIcon, ReplaceIcon, StarIcon, TrendingUpIcon, WindowsIcon };
