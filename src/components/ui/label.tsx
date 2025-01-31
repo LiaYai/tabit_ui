a815fbe844ee70b8fc012d1ea0a70e22b3856325
annmiers <annmiers@yandex.ru>
2025-01-31 23:45:09 +0300

    add scss variables from branch feat(tailwind.config.js) to component / fix text alignment

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -1,33 +1,8 @@
 import { cva, type VariantProps } from "class-variance-authority";
 import React, { ReactNode } from "react";
 
-const labelStyle = {
-	'--default-text': 'hsla(0, 0%, 100%, 1)',
-	'--default': 'hsla(211, 24%, 17%, 1)',
-	'--primary': 'hsla(220, 100%, 54%, 1)',
-	'--secondary': 'hsla(267, 100%, 60%, 1)',
-	'--info': 'hsla(189, 100%, 43%, 1)',
-	'--success': 'hsla(142, 71%, 45%, 1)',
-	'--warning': 'hsla(40, 100%, 50%, 1)',
-	'--error': 'hsla(11, 100%, 59%, 1)',
-	'--default-opacity': 'hsla(210, 13%, 62%, 0.16)',
-	'--primary-opacity': 'hsla(220, 100%, 54%, 0.16)',
-	'--secondary-opacity': 'hsla(267, 100%, 60%, 0.16)',
-	'--info-opacity': 'hsla(189, 100%, 43%, 0.16)',
-	'--success-opacity': 'hsla(142, 71%, 45%, 0.16)',
-	'--warning-opacity': 'hsla(40, 100%, 50%, 0.16)',
-	'--error-opacity': 'hsla(11, 100%, 59%, 0.16)',
-	'--default-opacity-text': 'hsla(208, 13%, 45%, 1)',
-	'--primary-opacity-text': 'hsla(220, 98%, 44%, 1)',
-	'--secondary-opacity-text': 'hsla(261, 76%, 41%, 1)',
-	'--info-opacity-text': 'hsla(198, 100%, 31%, 1)',
-	'--success-opacity-text': 'hsla(154, 78%, 31%, 1)',
-	'--warning-opacity-text': 'hsla(36, 100%, 36%, 1)',
-	'--error-opacity-text': 'hsla(2, 77%, 41%, 1)',
-} as React.CSSProperties;
-
 const labelVariants = cva(
-	"inline-flex items-center font-manrope h-[24px] justify-center rounded-[6px] px-[6px] py-[2px] text-[12px] font-bold leading-[20px] transition-colors gap-[6px]",
+	"inline-flex items-center h-[24px] justify-center rounded-[6px] px-[6px] text-[12px] font-bold transition-colors gap-[6px]",
 {
     variants: {
 		variant: {
@@ -49,107 +24,107 @@ const labelVariants = cva(
 		{
 			variant: "filled",
 			labelColor: "default",
-			class: "bg-[var(--default)] text-[var(--default-text)]",
+			class: "bg-grey-800 text-white",
 		},
 		{
 			variant: "filled",
 			labelColor: "primary",
-			class: "bg-[var(--primary)] text-[var(--default-text)]",
+			class: "bg-primary-main text-white",
 		},
 		{
 			variant: "filled",
 			labelColor: "secondary",
-			class: "bg-[var(--secondary)] text-[var(--default-text)]",
+			class: "bg-secondary-main text-white",
 		},
 		{
 			variant: "filled",
 			labelColor: "info",
-			class: "bg-[var(--info)] text-[var(--default-text)]",
+			class: "bg-info-main text-white",
 		},
 		{
 			variant: "filled",
 			labelColor: "success",
-			class: "bg-[var(--success)] text-[var(--default-text)]",
+			class: "bg-success-main text-white",
 		},
 		{
 			variant: "filled",
 			labelColor: "warning",
-			class: "bg-[var(--warning)] text-[var(--default)]",
+			class: "bg-warning-main text-grey-800",
 		},
 		{
 			variant: "filled",
 			labelColor: "error",
-			class: "bg-[var(--error)] text-[var(--default-text)]",
+			class: "bg-error-main text-white",
 		},
 		{
 			variant: "outlined",
 			labelColor: "default",
-			class: "border-[var(--default)] text-[var(--default)]",
+			class: "border-grey-800 text-grey-800",
 		},
 		{
 			variant: "outlined",
 			labelColor: "primary",
-			class: "border-[var(--primary)] text-[var(--primary)]",
+			class: "border-primary-main text-primary-main",
 		},
 		{
 			variant: "outlined",
 			labelColor: "secondary",
-			class: "border-[var(--secondary)] text-[var(--secondary)]",
+			class: "border-secondary-main text-secondary-main",
 		},
 		{
 			variant: "outlined",
 			labelColor: "info",
-			class: "border-[var(--info)] text-[var(--info)]",
+			class: "border-info-main text-info-main",
 		},
 		{
 			variant: "outlined",
 			labelColor: "success",
-			class: "border-[var(--success)] text-[var(--success)]",
+			class: "border-success-main text-success-main",
 		},
 		{
 			variant: "outlined",
 			labelColor: "warning",
-			class: "border-[var(--warning)] text-[var(--warning)]",
+			class: "border-warning-main text-warning-main",
 		},
 		{
 			variant: "outlined",
 			labelColor: "error",
-			class: "border-[var(--error)] text-[var(--error)]",
+			class: "border-error-main text-error-main",
 		},
 		{
 			variant: "soft",
 			labelColor: "default",
-			class: "bg-[var(--default-opacity)] text-[var(--default-opacity-text)]",
+			class: "bg-grey-transparent16 text-grey-600",
 		},
 		{
 			variant: "soft",
 			labelColor: "primary",
-			class: "bg-[var(--primary-opacity)] text-[var(--primary-opacity-text)]",
+			class: "bg-primary-transparent16 text-primary-dark",
 		},
 		{
 			variant: "soft",
 			labelColor: "secondary",
-			class: "bg-[var(--secondary-opacity)] text-[var(--secondary-opacity-text)]",
+			class: "bg-secondary-transparent16 text-secondary-dark",
 		},
 		{
 			variant: "soft",
 			labelColor: "info",
-			class: "bg-[var(--info-opacity)] text-[var(--info-opacity-text)]",
+			class: "bg-info-transparent16 text-info-dark",
 		},
 		{
 			variant: "soft",
 			labelColor: "success",
-			class: "bg-[var(--success-opacity)] text-[var(--success-opacity-text)]",
+			class: "bg-success-transparent16 text-success-dark",
 		},
 		{
 			variant: "soft",
 			labelColor: "warning",
-			class: "bg-[var(--warning-opacity)] text-[var(--warning-opacity-text)]",
+			class: "bg-warning-transparent16 text-warning-dark",
 		},
 		{
 			variant: "soft",
 			labelColor: "error",
-			class: "bg-[var(--error-opacity)] text-[var(--error-opacity-text)]",
+			class: "bg-error-transparent16 text-error-dark",
 		},
     ],
     defaultVariants: {
@@ -159,16 +134,14 @@ const labelVariants = cva(
 }
 );
 
-// Типы для пропсов
 interface LabelProps
 	extends React.HTMLAttributes<HTMLSpanElement>,
     VariantProps<typeof labelVariants> {
 	children: ReactNode;
-	iconBefore?: ReactNode; // Иконка перед текстом
-	iconAfter?: ReactNode; // Иконка после текста
+	iconBefore?: ReactNode;
+	iconAfter?: ReactNode;
 }
 
-// Компонент Label
 const Label = ({
 	children,
 	variant,
@@ -179,19 +152,19 @@ const Label = ({
 	...props
 }: LabelProps) => {
 	return (
-		<span
+		<div 
 			className={labelVariants({ variant, labelColor, className })}
-			style={labelStyle}
 			data-testid="Label"
 			{...props}
 		>
 			{iconBefore && <span>{iconBefore}</span>}
-			{children}
+			<span className="leading-[2]">
+				{children}
+			</span>
 			{iconAfter && <span>{iconAfter}</span>}
-		</span>
+		</div>
 	);
 };
-
 
 Label.displayName = 'Label';
 
